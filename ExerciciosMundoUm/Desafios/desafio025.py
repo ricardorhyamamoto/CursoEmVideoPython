9dfec5a320fb546590fb9fdc17f241543847c1c3
ricardorhyamamoto <ricardo.yamamoto@outlook.com>
2024-06-19 22:50:28 -0300

    Alteração sendo feito no notebook Linux.

--- ExerciciosMundoUm/Desafios/desafio025.py
+++ ExerciciosMundoUm/Desafios/desafio025.py
@@ -2,3 +2,4 @@
 
 #Só vou fazer uma alteração nesse arquivo para validar se a configuração do Git, VSCode, Python no notebook Windows deu certo.
 
+#Essa alteração agora está sendo feita no notebook Linux.
