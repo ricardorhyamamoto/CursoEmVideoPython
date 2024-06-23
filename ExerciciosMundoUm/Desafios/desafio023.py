f8731519deb940bc29545b4682716b6eb83a8092
ricardorhyamamoto <ricardo.yamamoto@outlook.com>
2024-06-23 14:30:34 -0300

    Resolvendo o desafio 23

--- ExerciciosMundoUm/Desafios/desafio023.py
+++ ExerciciosMundoUm/Desafios/desafio023.py
@@ -1,9 +1,15 @@
 # FAÇA UM PROGRAMA QUE LEIA UM  NÚMERO DE 0 A 9999 E MOSTRE NA TELA CADA UM DOS DÍGITOS SEPARADOS EM UNIDADE, DEZENA, CENTENA E MILHAR
 
-numero = input('Digite um número entre 0 e 9999: ')
+numero = int(input('Digite um número entre 0 e 9999: '))
 
-print(numero[0])
-print(numero[1])
-print(numero[2])
-print(numero[3])
+unidade = numero // 1 % 10
+dezena = numero // 10 % 10
+centena = numero // 100 % 10
+milhar = numero // 1000 % 10
 
+print('Analisando o número: {}'.format(numero))
+
+print('Unidade: {}'.format(unidade))
+print('Dezena: {}'.format(dezena))
+print('Centena: {}'.format(centena))
+print('Milhar: {}'.format(milhar))
